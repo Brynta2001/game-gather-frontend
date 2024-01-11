1d7b542ffb6e50f87c081aed1ebaaf45e919fc8d
Mateo Pérez <cristopher.perez01@epn.edu.ec>
2024-01-11 07:21:02 -0500

    fix: add a lot of things

--- src/games/edit.ts
+++ src/games/edit.ts
@@ -1,7 +1,8 @@
 import { axiosInstance } from "../utils/axios";
 
 const id = new URLSearchParams(window.location.search).get('id')!;
-console.log(id);
+const modalTitle = document.getElementById('staticBackdropLabel') as HTMLHeadingElement;
+const modalMessage = document.getElementById('modal-message') as HTMLParagraphElement;
 
 const getGame = async (id: number) => {
     const game = await axiosInstance.get(`/games/${id}`);
@@ -69,17 +70,17 @@ const edit = async (editGame: any) => {
         }
     }).then((response) => {
         console.log(response);
-        /*modalTitle.textContent = 'Success';
-        modalMessage.textContent = 'Game created successfully';
+        modalTitle.textContent = 'Success';
+        modalMessage.textContent = 'Game edited successfully';
         $('#staticBackdrop').modal('show');
         $('#staticBackdrop').on('hidden.bs.modal', function () {
             // Redirigir a la página login.html
             window.location.href = '../games/dashboard.html';
-        });*/
+        });
     }).catch((error) => {
         console.log(error);
-        /*modalTitle.textContent = 'Error creating user';
+        modalTitle.textContent = 'Error editing game';
         modalMessage.textContent = error.response?.data?.message;
-        $('#staticBackdrop').modal('show');*/
+        $('#staticBackdrop').modal('show');
     });
 };
--- src/games/game.ts
+++ src/games/game.ts
@@ -1,6 +1,9 @@
 import { axiosInstance } from "../utils/axios";
 
 const gameEdit = document.querySelector<HTMLAnchorElement>('#game-reference-edit')!;
+const gameDelete = document.querySelector<HTMLAnchorElement>('#game-reference-delete')!;
+const modalTitle = document.getElementById('staticBackdropLabel') as HTMLHeadingElement;
+const modalMessage = document.getElementById('modal-message') as HTMLParagraphElement;
 
 const getGame = async (id: number) => {
     const game = await axiosInstance.get(`/games/${id}`);
@@ -21,3 +24,21 @@ const renderGame = async () => {
 };
 
 renderGame();
+
+gameDelete.addEventListener('click', async (event) => {
+    event.preventDefault();
+    const id = new URLSearchParams(window.location.search).get('id')!;
+    await drop(+id);
+    modalTitle.textContent = 'Success';
+    modalMessage.textContent = 'Game deleted successfully';
+    $('#staticBackdrop').modal('show');
+    $('#staticBackdrop').on('hidden.bs.modal', function () {
+        // Redirigir a la página login.html
+        window.location.href = '../games/dashboard.html';
+    });
+});
+
+const drop = async (id: number) => {
+    const game = await axiosInstance.delete(`/games/${id}`);
+    return game.data;
+};
